662bd72482313b8b200c1eeb9dbec3364078e9ae
Cameron Wright <arabia.cw@gmail.com>
2020-03-05 15:50:57 -0500

    hangman
    w

--- src/main.java
+++ src/main.java
@@ -1,3 +1,37 @@
+
+import java.util.Scanner;
 public class main {
-    
+
+
+    public static final String[] WORDS = {
+            "ABSTRACT", "ASSERT", "BOOLEAN", "BREAK", "BYTE",
+            "CASE", "CATCH", "CHAR", "CLASS", "CONST",
+            "CONTINUE", "DEFAULT", "DOUBLE", "DO", "ELSE",
+            "ENUM", "EXTENDS", "FALSE", "FINAL", "FINALLY",
+            "FLOAT", "FOR", "GOTO", "IF", "IMPLEMENTS",
+            "IMPORT", "INSTANCEOF", "INT", "INTERFACE",
+            "LONG", "NATIVE", "NEW", "NULL", "PACKAGE",
+            "PRIVATE", "PROTECTED", "PUBLIC", "RETURN",
+            "SHORT", "STATIC", "STRICTFP", "SUPER", "SWITCH",
+            "SYNCHRONIZED", "THIS", "THROW", "THROWS",
+            "TRANSIENT", "TRUE", "TRY", "VOID", "VOLATILE", "WHILE"
+    };
+
+
+
+    public static void main(String[] args){
+        System.out.println("Welcome to HANGMAN!!");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
